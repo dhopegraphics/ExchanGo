4a8951f58169eeece76b176f4b28a47148796958
dhopegraphics <dhopegraphics@gmail.com>
2025-06-17 04:51:48 +0000

    refactor: remove unused custom bottom tab components and simplify tab layout

--- app/(main)/(tabs)/_layout.jsx
+++ app/(main)/(tabs)/_layout.jsx
@@ -1,15 +1,11 @@
 import { Tabs } from "expo-router";
-import { CustomBottomTab } from "@/components/BottomTabs/CustomBottomTab";
 import { StatusBar } from "expo-status-bar";
 
 export default function TabLayout() {
   return (
     <>
       <StatusBar style="dark" />
-      <Tabs
-        screenOptions={{ tabBarActiveTintColor: "blue" }}
-        tabBar={(props) => <CustomBottomTab {...props} />}
-      >
+      <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
         <Tabs.Screen name="Explore" options={{ headerShown: false }} />
         <Tabs.Screen name="SwapCenter" options={{ headerShown: false }} />
         <Tabs.Screen name="Communities" />
